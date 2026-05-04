9084eaa961b8831be994af6412e4a46d3420969e
proganurag101 <anurag94530@gmail.com>
2026-05-05 00:19:49 +0530

    Commit:4thMay2026: added testNg: DataProvider practice example

--- src/test/java/com/learn/maven/JavaProviderTest.java
+++ src/test/java/com/learn/maven/JavaProviderTest.java
@@ -1,5 +1,23 @@
 package com.learn.maven;
 
-public class JavaProviderTest {
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+public class JavaProviderTest {
+    @Test(dataProvider = "getData") //method where reference for dataProvider Class.
+    public void testLogin(String Username,String Password){
+        System.out.println("======START=====");
+        System.out.println("Username is: "+Username+"Password is: "+Password);
+        System.out.println("======END=======");
+    }
+
+    @DataProvider
+    public Object[][] getData(){   //Object is java inbuilt class
+        return new Object[][]{     //return with same data type
+                {"User1 ", "Password1"},
+                {"User2 ", "Password2"},
+                {"User3 ", "Password3"},
+                {"User4 ", "Password4"}
+        };
+    }
 }
