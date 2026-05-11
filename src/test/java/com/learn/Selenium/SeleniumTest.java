0f691dd3dfdee078d1beb1c8bba27e7dc39453e7
proganurag101 <anurag94530@gmail.com>
2026-05-11 23:17:52 +0530

    Commit:11thMay2026: added SeleniumTest: basic driver file

---
+++ src/test/java/com/learn/Selenium/SeleniumTest.java
@@ -0,0 +1,22 @@
+package com.learn.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class SeleniumTest {
+
+    @Test
+    public void seleniumTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/browse");
+        String title = driver.getTitle();
+        System.out.println("Your Title is : "+ title);
+        Assert.assertEquals("Netflix",title,"Title incorrect!");
+        driver.close();
+    }
+
+}
