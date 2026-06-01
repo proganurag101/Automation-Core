7e8c837dc19957fc3edc98d228430f5289a899c9
proganurag101 <anurag94530@gmail.com>
2026-06-02 00:02:01 +0530

    Commit:2nd June2026: added webInteractions using selenium

--- src/test/java/com/learn/Selenium/InteractionsTest.java
+++ src/test/java/com/learn/Selenium/InteractionsTest.java
@@ -2,7 +2,11 @@ package com.learn.Selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InteractionsTest extends BaseTest{
@@ -49,6 +53,90 @@ public class InteractionsTest extends BaseTest{
         WebElement ConfirmationHeader = driver.findElement(By.xpath("//p[@class='lead']"));
         assertThat(ConfirmationHeader.getText()).contains("This site contains a collection of sample web pages to be tested with");
     }
+
+    @Test
+    public void testCheckBoxes(){
+        driver.get(url);
+        WebElement Checkbox = driver.findElement(By.cssSelector("[type=\"checkbox\"]:checked"));
+        assertThat(Checkbox.getDomAttribute("id")).isEqualTo("my-check-1");
+        assertThat(Checkbox.isSelected()).isTrue(); //is selected is used on radiobutton and check box
+
+        //unchecked box
+        WebElement uncheckedBox = driver.findElement(By.cssSelector("[type=\"checkbox\"]:not(:checked)"));
+        assertThat(uncheckedBox.getDomAttribute("id")).isEqualTo("my-check-2");
+        assertThat(uncheckedBox.isSelected()).isFalse();
+
+        //for checkBox already selected:
+        Checkbox.click(); //got unselected afterClick
+        assertThat(Checkbox.isSelected()).isFalse();
+
+        //for uncheckedBox after click
+        uncheckedBox.click();
+        assertThat(uncheckedBox.isSelected()).isTrue();
+
+    }
+
+    @Test
+    public void testRadioButton(){
+        driver.get(url);
+        WebElement radioButton1 = driver.findElement(By.xpath("//input[@type=\"radio\"  and @checked]"));
+        assertThat(radioButton1.getDomAttribute("id")).isEqualTo("my-radio-1");
+        //is already checked by default
+        assertThat(radioButton1.isSelected()).isTrue();
+
+        WebElement radioButton2 = driver.findElement(By.xpath("//input[@type=\"radio\"  and not(@checked)]"));
+        assertThat(radioButton2.getDomAttribute("id")).isEqualTo("my-radio-2");
+        assertThat(radioButton2.isSelected()).isFalse();
+
+        //after click radioButton2 is selected and 1 is auto unselected
+        radioButton2.click();
+        assertThat(radioButton1.isSelected()).isFalse();
+        assertThat(radioButton2.isSelected()).isTrue();
+    }
+
+    @Test
+    public void testDropDowns(){
+        driver.get(url);
+        WebElement listBox = driver.findElement(By.name("my-select"));
+
+        //exclusive class for select
+        Select select = new Select(listBox); //new object- select,for the listbox
+        List<WebElement> options =  select.getOptions();
+
+        for (WebElement option : options){
+            System.out.println(option.getText());
+        }
+        //select on of the visible text from dropdown like one,two,three present there
+        String optionValue = "Three";
+        select.selectByVisibleText(optionValue); // we selected three
+        assertThat(select.getFirstSelectedOption().getText()).isEqualTo(optionValue); // as 3 was selected earlier so out expected value matches
+
+    }
+
+    @Test
+    public void testDataList(){
+        driver.get(url);
+        WebElement dataList = driver.findElement(By.name("my-datalist"));
+        assertThat(dataList.getDomAttribute("list")).isEqualTo("my-options");
+        dataList.click();
+
+        WebElement option = driver.findElement(By.xpath("//datalist/option[2]")); //returns new york
+        String optionValue = option.getDomAttribute("Value");  //fetching new york from option
+        dataList.sendKeys(optionValue);
+
+        assertThat(optionValue).isEqualTo("New York");
+
+
+
+
+
+
+
+
+
+
+    }
+
 
 
 }
