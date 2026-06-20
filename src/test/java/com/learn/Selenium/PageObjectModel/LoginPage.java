3c6994b3a9c1402d2780b2755703e647217b0625
proganurag101 <anurag94530@gmail.com>
2026-06-20 19:58:24 +0530

    Commit:20th June2026: added POM with selenium handling code.

---
+++ src/test/java/com/learn/Selenium/PageObjectModel/LoginPage.java
@@ -0,0 +1,52 @@
+package com.learn.Selenium.PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+//page Object class
+//write once then keep using
+public class LoginPage {
+    private WebDriver driver;
+
+    //Locators
+    //below By is an object and variables are its instances.
+    By usernameInput = By.id("username");
+    By passwordInput = By.id("password");
+    By submitButton = By.cssSelector("button");
+    By successMessage = By.id("success");
+    By errorMessage = By.id("invalid");
+
+    //constructor
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    //Actions
+    public void login(String userName,String password){
+        this.enterUsername(userName);
+        this.enterPassword(password);
+        this.clickLogin();
+    }
+    public boolean isLoginSuccessful(){
+        return driver.findElement(successMessage).isDisplayed();
+    }
+    public boolean isErrorDisplayed(){
+        return driver.findElement(errorMessage).isDisplayed();
+    }
+    private void enterUsername(String userName){
+        driver.findElement(usernameInput).sendKeys(userName);
+    }
+
+    private void enterPassword(String password){
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    private void clickLogin(){
+        driver.findElement(submitButton).click();
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/learn/Selenium/PageObjectModel/LoginPageTests.java
@@ -0,0 +1,57 @@
+package com.learn.Selenium.PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class LoginPageTests {
+    WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/login-form.html");
+    }
+
+    @Test
+    public void testLoginNormalApproach(){
+        WebElement login = driver.findElement(By.id("username"));
+        login.sendKeys("user");
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("user");
+
+        WebElement submit = driver.findElement(By.cssSelector("button"));
+        submit.click();
+
+        WebElement success = driver.findElement(By.id("success"));
+        assertThat(success.getText()).isEqualTo("Login successful");
+    }
+
+    @Test
+    public void testValidLoginWithPageObjectApproach(){
+        driver.manage().window().maximize();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("user","user");
+        assertThat(loginPage.isLoginSuccessful()).isTrue();
+    }
+
+    @Test
+    public void testInValidLoginWithPageObjectApproach(){
+        driver.manage().window().maximize();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("user23","user");
+        assertThat(loginPage.isErrorDisplayed()).isTrue();
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
