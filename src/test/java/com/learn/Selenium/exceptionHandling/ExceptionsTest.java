609536bbaa6aac41e4e4f5db7b90628e6cc1d90e
proganurag101 <anurag94530@gmail.com>
2026-06-30 09:01:54 +0530

    Commit:30th June2026: added  ExceptionHandling with selenium handling code.

--- src/test/java/com/learn/Selenium/exceptionHandling/ExceptionsTest.java
+++ src/test/java/com/learn/Selenium/exceptionHandling/ExceptionsTest.java
@@ -1,7 +1,9 @@
 package com.learn.Selenium.exceptionHandling;
 
 import com.learn.Selenium.BaseTest;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -11,7 +13,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ExceptionsTest extends BaseTest {
     @Test
-    public void noSuchElementExceptionTest(){
+    public void noSuchElementExceptionTest() {
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 //        //incorrect id
 //        WebElement element = driver.findElement(By.id("my-text-if"));
@@ -24,26 +26,49 @@ public class ExceptionsTest extends BaseTest {
 
 
     @Test
-    public void noSuchElementExceptionTest2(){
+    public void noSuchElementExceptionTest2() {
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
         driver.manage().window().maximize();
 
-        //added implicit wait
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebElement landscape = driver.findElement(By.id("landscape"));
         //above element doesn't load immidiately and cause issue
-        //so we add explicit/implicit.
+        //Fix: Add implicit/explicit waits
         assertThat(landscape.getDomProperty("src")).containsIgnoringCase("landscape");
-
-
 
     }
 
     @Test
-    public void testElementNotInteractableException(){
+    public void testElementNotInteractableException() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+        driver.manage().window().maximize();
+
+        WebElement element = driver.findElement(By.xpath("//input[@type='hidden']"));
+
+        try {
+            element.click();
+        } catch (ElementNotInteractableException exception) {
+            System.out.println("Unable to click on the element");
+            //Handle such alert by try catch
+            //add rest of the steps to fix such error
+            //ex removing type='hidden' for handling hidden element
+        }
+
 
     }
+    @Test
+    public void testNoAlertPresentException(){
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/dialog-boxes.html");
 
+//        driver.findElement(By.id("my-alert")).click();
+        Alert alert = driver.switchTo().alert();
+        assertThat(alert.getText()).isEqualTo("Hello world!");
+        alert.accept();
+
+        //Fix: By adding proper locator to reach the alert,
+        //Here we didn't identify the alert tab
+    }
 
 
 }
