857906a1c4b60c59fefbfa19afb61a68b8539f98
proganurag101 <anurag94530@gmail.com>
2026-07-12 14:52:25 +0530

    Commit: 12th July2026: added dataType handling code in cucumber

--- src/test/java/com/learn/runner/CartTestRunner.java
+++ src/test/java/com/learn/runner/CartTestRunner.java
@@ -3,20 +3,16 @@ package com.learn.runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.learn.StepDefinitions",
+        tags = "@Cart",
+        plugin = {"pretty","html:target/cucumber-reports.html"}
+
+)
 public class CartTestRunner {
-
-
-    @RunWith(Cucumber.class)
-    @CucumberOptions(
-            features = "src/test/resources/features",
-            glue = "com.learn.StepDefinitions",
-            tags = "@LoginParameter ",
-            plugin = {"pretty","html:target/cucumber-reports.html"}
-
-    )
-    public class TestRunner {
-    }
 
 }
--- src/test/java/com/learn/runner/TestRunner.java
+++ src/test/java/com/learn/runner/TestRunner.java
@@ -1,15 +1,15 @@
 package com.learn.runner;
 
-
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.learn.StepDefinitions",
-        tags = "@Cart",
+        tags = "@LoginParameter",
         plugin = {"pretty","html:target/cucumber-reports.html"}
 
 )
