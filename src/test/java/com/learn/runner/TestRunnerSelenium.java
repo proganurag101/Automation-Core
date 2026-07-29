51471ba19c451488f3c7e98e4807f2a77aa01ce9
proganurag101 <anurag94530@gmail.com>
2026-07-29 23:59:52 +0530

    Commit: 29th July2026: added restAssured,Selenium code in cucumber

--- src/test/java/com/learn/runner/TestRunnerSelenium.java
+++ src/test/java/com/learn/runner/TestRunnerSelenium.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.learn.StepDefinitions",
-        tags = "@Selenium",
+        tags = "@Selenium or @api",
         plugin = {"pretty","html:target/cucumber-reports.html"},
         monochrome = true
 
