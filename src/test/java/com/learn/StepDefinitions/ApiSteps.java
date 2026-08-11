dee10b580ef5bf0e5c22130167d767301f18046e
proganurag101 <anurag94530@gmail.com>
2026-08-11 21:52:57 +0530

    Commit: 11 August 2026: added tag for running in cucumber and created Jenkins setup,made changes in pom.xml to run by Jenkins.

--- src/test/java/com/learn/StepDefinitions/ApiSteps.java
+++ src/test/java/com/learn/StepDefinitions/ApiSteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
 import static org.hamcrest.Matchers.equalTo;
 
 public class ApiSteps {
@@ -18,17 +19,18 @@ public class ApiSteps {
 
     @When("a GET request is sent to {string}")
     public void a_get_request_is_sent_to(String userId) {
-       response = RestAssured.get("/users/"+ userId);
+        response = RestAssured.get("/users/" + userId);
+        //resulting url = baseUrl + /users/+1 = https://jsonplaceholder.typicode.com/users/1
     }
 
     @Then("the response code should be {int}")
     public void the_response_code_should_be(Integer expectedStausCode) {
-     response.then().statusCode(expectedStausCode);
+        response.then().statusCode(expectedStausCode);
     }
 
     @Then("the response body should contain the username {string}")
     public void the_response_body_should_contain_the_username(String expectedUsername) {
-        response.then().body("username",equalTo(expectedUsername));
+        response.then().body("username", equalTo(expectedUsername));
     }
 
 }
--- src/test/java/com/learn/runner/CartTestRunnerJSONreport.java
+++ src/test/java/com/learn/runner/CartTestRunnerJSONreport.java
@@ -4,12 +4,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.learn.StepDefinitions",
-        tags = "@cartDataTableTest",
+        tags = "@Cart",
         plugin = {"pretty","json:target/cucumber-reports.json"}
 
 )
