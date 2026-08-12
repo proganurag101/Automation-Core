0ca9d00541b46f7f229e625850679d06146eebc9
proganurag101 <anurag94530@gmail.com>
2026-08-12 23:54:08 +0530

    Commit: 12 August 2026: added tag for running in cucumber and created Jenkins setup,made changes in pom.xml to run by Jenkins,learning build params on Jenkins

--- src/test/java/com/learn/runner/CartTestRunner.java
+++ src/test/java/com/learn/runner/CartTestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.learn.StepDefinitions","com.learn.hooks"},
-        tags = "@Cart or @backgroundTest or @Smoke",
+        tags = "@Cart or @backgroundTest",
         plugin = {"pretty","html:target/cucumber-reports.html"}
 
 )
