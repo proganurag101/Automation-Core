cb6c710894ccfc40a1683543db64f4c66134c0cf
proganurag101 <anurag94530@gmail.com>
2026-08-14 22:36:19 +0530

    Commit: 14 August 2026: added Jenkins File

--- src/test/java/com/learn/runner/ApiRunner.java
+++ src/test/java/com/learn/runner/ApiRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.learn.StepDefinitions"},
-        tags = "@api",
+        tags = "@api or @Selenium",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
