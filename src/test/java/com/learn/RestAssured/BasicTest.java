36bcc073455e4b46874f8b1dcc008ddd82f6d2e3
proganurag101 <anurag94530@gmail.com>
2026-09-06 21:40:07 +0530

    6th Sept: Practice: Added RestAssured Get Call validations.

--- src/test/java/com/learn/RestAssured/BasicTest.java
+++ src/test/java/com/learn/RestAssured/BasicTest.java
@@ -8,11 +8,47 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class BasicTest {
 
     @Test
-    public void testGet(){
-        given()
-                .baseUri("http://api.zippopotam.us")
-                .when().get("/us/90210")
-                .then().statusCode(200)
-                .body("country",equalTo("United States"));
+    public void testGet() {
+        given().baseUri("http://api.zippopotam.us")
+                .when().get("us/90210")
+                .then().statusCode(200);
     }
+
+    @Test
+    public void testCountryDetails() {
+        given().baseUri("http://api.zippopotam.us")
+                .when().get("us/90210")
+                .then().statusCode(200)
+                .body("country", equalTo("United States"))
+                //Note: For keys with spaces,we keep them in single quote inside a double quote.
+                .body("'country abbreviation'", equalTo("US"));
+
+    }
+
+    @Test
+    public void VerifyPlacesDetails() {
+        given().baseUri("http://api.zippopotam.us")
+                .when().get("us/90210")
+                .then().statusCode(200)
+                .body("places[0].state", equalTo("California"))
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
+                .body("places[0].longitude", equalTo("-118.4065"));
+    }
+
+    @Test
+    public void verifyNegativeTest() {
+        given().baseUri("http://api.zippopotam.us")
+                .when().get("us/22881")
+                .then().statusCode(404);
+    }
+
+    @Test
+    public void validateResponseType() {
+        given().baseUri("http://api.zippopotam.us")
+                .when().get("us/90210")
+                .then().statusCode(200)
+                //use equalTo to avoid false positives
+                .contentType(equalTo("application/json"));
+    }
+
 }
