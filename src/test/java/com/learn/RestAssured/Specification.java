2c3b24939474744ec1d38c06e0d749ee72797fd0
proganurag101 <anurag94530@gmail.com>
2026-09-06 22:54:25 +0530

    6th Sept: Practice: Added RestAssured: Specification validations.

---
+++ src/test/java/com/learn/RestAssured/Specification.java
@@ -0,0 +1,72 @@
+package com.learn.RestAssured;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+//1->RequestSpecification reqSpec = new RequestSpecBuilder()
+//2->Write expected default spec,
+//3->Do .build()
+//4->Do given.spec(reqSpec).
+
+public class Specification {
+    RequestSpecification reqSpec = new RequestSpecBuilder()
+            .setBaseUri("http://api.zippopotam.us")
+            .setContentType("application/json")
+            .build();
+
+    ResponseSpecification respSpec = new ResponseSpecBuilder()
+            .expectContentType("application/json")
+            .expectStatusCode(200)
+            .expectResponseTime(lessThan(2000L))
+            .build();
+
+
+    @Test
+    public void testGet() {
+        given().spec(reqSpec)
+                .when().get("us/90210")
+                .then().spec(respSpec);
+    }
+
+    @Test
+    public void testCountryDetails() {
+        given().spec(reqSpec)
+                .when().get("us/90210")
+                .then().spec(respSpec)
+                .body("country", equalTo("United States"))
+                //Note: For keys with spaces,we keep them in single quote inside a double quote.
+                .body("'country abbreviation'", equalTo("US"));
+
+    }
+
+    @Test
+    public void VerifyPlacesDetails() {
+        given().spec(reqSpec)
+                .when().get("us/90210")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo("California"))
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
+                .body("places[0].longitude", equalTo("-118.4065"));
+    }
+
+    @Test
+    public void verifyNegativeTest() {
+        given().spec(reqSpec)
+                .when().get("us/22881")
+                .then().statusCode(404);
+    }
+
+    @Test
+    public void validateResponseType() {
+        given().spec(reqSpec)
+                .when().get("us/90210")
+                .then().spec(respSpec);
+    }
+}
