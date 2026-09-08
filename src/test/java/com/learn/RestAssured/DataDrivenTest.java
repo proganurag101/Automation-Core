3536cdc4dc05676d2914d68832fd13731f4b3d04
proganurag101 <anurag94530@gmail.com>
2026-09-08 23:13:01 +0530

    8th Sept: Practice: Added RestAssured: DataDrivenTests and Logging handling.

---
+++ src/test/java/com/learn/RestAssured/DataDrivenTest.java
@@ -0,0 +1,101 @@
+package com.learn.RestAssured;
+
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+
+public class DataDrivenTest {
+
+    RequestSpecification reqSpec = new RequestSpecBuilder()
+            .setBaseUri("http://api.zippopotam.us")
+            .setContentType("application/json")
+            .build();
+
+    ResponseSpecification respSpec = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .expectContentType("application/json")
+            .expectResponseTime(lessThan(3000L))
+            .build();
+
+
+    //Manual Data Driven test
+    @Test
+    public void testPlaces() {
+        given().spec(reqSpec)
+                .when().get("us/90210")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo("California"));
+
+    }
+
+    @Test
+    public void testPlaces2() {
+        given().spec(reqSpec)
+                .when().get("us/12345")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo("New York"));
+
+    }
+
+    @Test
+    public void testPlaces3() {
+        given().spec(reqSpec)
+                .when().get("us/24848")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo("West Virginia"));
+
+    }
+
+    @Test
+    public void testPlaces4() {
+        given().spec(reqSpec)
+                .when().get("in/228001")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo("Uttar Pradesh"));
+
+    }
+
+    //TestNg Based
+    @Test(dataProvider = "zipCodeData")
+    public void verifyStates(String countryCode, String zipCode, String State) {
+        given().spec(reqSpec)
+                .when().get("/" + countryCode + "/" + zipCode)
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo(State));
+    }
+
+    //or
+
+    //TestNg Based
+    @Test(dataProvider = "zipCodeData")
+    public void verifyStatesParam(String countryCode, String zipCode, String State) {
+        given().spec(reqSpec)
+                .pathParam("countryCode", countryCode)
+                .pathParam("zipCode", zipCode)
+                .when().get("/{countryCode}/{zipCode}")
+                .then().spec(respSpec)
+                .body("places[0].state", equalTo(State));
+    }
+
+
+    @DataProvider(name = "zipCodeData")
+    public Object[][] getZipCodes() {
+        return new Object[][]{
+                {"us", "90210", "California"},
+                {"us", "12345", "New York"},
+                {"us", "24848", "West Virginia"},
+                {"in", "228001", "Uttar Pradesh"},
+
+        };
+    }
+
+
+}
---
+++ src/test/java/com/learn/RestAssured/LogTest.java
@@ -0,0 +1,30 @@
+package com.learn.RestAssured;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class LogTest {
+
+    @Test
+    public void testAllLog() {
+        given().baseUri("http://api.zippopotam.us").log().all()
+                .when().get("us/90210")
+                .then().statusCode(200).log().all();
+    }
+
+    @Test
+    public void testSpecificLog(){
+        given().baseUri("http://api.zippopotam.us")
+                .log().method().and()
+                .log().uri().and()
+                .log().headers()
+
+                .when().get("us/90210")
+                .then()
+                .log().body().and()
+                .log().headers().and()
+                .log().status().and()
+                .statusCode(200);
+    }
+}
