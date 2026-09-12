92f9483be5efbcfb44cd2e721cf52e5666f9ff43
proganurag101 <anurag94530@gmail.com>
2026-09-12 22:00:34 +0530

    11th Sept: Practice: Added RestAssured: CRUD operation.

--- src/test/java/com/learn/RestAssured/CRUD.java
+++ src/test/java/com/learn/RestAssured/CRUD.java
@@ -9,37 +9,42 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured.*;
 
 import static io.restassured.RestAssured.given;
-
+//32:20
 public class CRUD {
-
+    String id;
     @BeforeClass
     public void setUp(){
         RestAssured.baseURI = "https://petstore.swagger.io";
         RestAssured.basePath= "/v2";
         RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setContentType("application/json").build();
+                .setContentType("application/json")
+                .build();
     }
 
     @Test
     public void testPost() {
-        String requestBody = "{\"id\":7772,\"category\":{\"id\":0,\"name\":\"Jack102\"},\"name\":\"doggie\",\"photoUrls\":[\"string\"],\"tags\":[{\"id\":2,\"name\":\"string\"}],\"status\":\"available\"}";
+
+        String requestBody = "{\"category\":{\"id\":0,\"name\":\"Jack102\"},\"name\":\"doggie\",\"photoUrls\":[\"string\"],\"tags\":[{\"id\":2,\"name\":\"string\"}],\"status\":\"available\"}";
 
         Response response = given().body(requestBody).
                 when().post("/pet");
-        String id = response.path("id").toString();
-        System.out.println("New pet Created"+id);
+        //Takes id value from response and convert it to string like id:211
+        //then makes it:  id = "211"
+        id = response.path("id").toString();
+        System.out.println("New pet Created: "+id);
 
     }
-    @Test
+    //depends on method makes sure,prev. method is ran successfully.
+    @Test(dependsOnMethods = "testPost")
     public void testGet(){
-        RestAssured.get("/pet/7772").then()
+        given().when().get("/pet/"+id).then() //or just do RestAssured.get("/pet/"+id).then()
                 .statusCode(200)
                 .log().body();
     }
 
-    @Test
+    @Test(dependsOnMethods = "testGet")
     public void testDelete(){
-        RestAssured.delete("/pet/7772").then()
+        RestAssured.delete("/pet/"+id).then()
                 .statusCode(200)
                 .log().body();
     }
